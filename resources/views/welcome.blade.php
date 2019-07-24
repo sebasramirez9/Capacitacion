7e2db8a2ffd74e7161f20afd7137756a4fef262e
sebas <sebastianramirezdiaz06@gmai.com>
2019-07-23 22:39:22 -0500

    I add the first design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    SISTEMAS
                 </div>
 
                 <div class="links">
