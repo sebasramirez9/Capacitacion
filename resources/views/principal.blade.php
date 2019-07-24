c15f9dca6d6e479d9ed76cb61f8119ae05807c85
sebas <sebastianramirezdiaz06@gmai.com>
2019-07-24 17:01:06 -0500

    Se implemento laravelMix y Vuejs

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -11,13 +11,13 @@
     <link rel="shortcut icon" href="img/favicon.png">
     <title>Sistema Ventas - IncanatoIT</title>
     <!-- Icons -->
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/simple-line-icons.min.css" rel="stylesheet">
-    <!-- Main styles for this application -->
-    <link href="css/style.css" rel="stylesheet">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="css/template.css" rel="stylesheet">
+
 </head>
 
 <body class="app header-fixed sidebar-fixed aside-menu-fixed aside-menu-hidden">
+<div id="app">
 <header class="app-header navbar">
     <button class="navbar-toggler mobile-sidebar-toggler d-lg-none mr-auto" type="button">
         <span class="navbar-toggler-icon"></span>
@@ -71,101 +71,20 @@
 </header>
 
 <div class="app-body">
-    <div class="sidebar">
-        <nav class="sidebar-nav">
-            <ul class="nav">
-                <li class="nav-item">
-                    <a class="nav-link active" href="main.html"><i class="icon-speedometer"></i> Escritorio</a>
-                </li>
-                <li class="nav-title">
-                    Mantenimiento
-                </li>
-                <li class="nav-item nav-dropdown">
-                    <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-bag"></i> Almacén</a>
-                    <ul class="nav-dropdown-items">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#"><i class="icon-bag"></i> Categorías</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#"><i class="icon-bag"></i> Artículos</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item nav-dropdown">
-                    <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-wallet"></i> Compras</a>
-                    <ul class="nav-dropdown-items">
-                        <li class="nav-item">
-                            <a class="nav-link" href="i#"><i class="icon-wallet"></i> Ingresos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#"><i class="icon-notebook"></i> Proveedores</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item nav-dropdown">
-                    <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-basket"></i> Ventas</a>
-                    <ul class="nav-dropdown-items">
-                        <li class="nav-item">
-                            <a class="nav-link" href="i#"><i class="icon-basket-loaded"></i> Ventas</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#"><i class="icon-notebook"></i> Clientes</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item nav-dropdown">
-                    <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-people"></i> Acceso</a>
-                    <ul class="nav-dropdown-items">
-                        <li class="nav-item">
-                            <a class="nav-link" href="i#"><i class="icon-user"></i> Usuarios</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#"><i class="icon-user-following"></i> Roles</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item nav-dropdown">
-                    <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-pie-chart"></i> Reportes</a>
-                    <ul class="nav-dropdown-items">
-                        <li class="nav-item">
-                            <a class="nav-link" href="i#"><i class="icon-chart"></i> Reporte Ingresos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#"><i class="icon-chart"></i> Reporte Ventas</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="main.html"><i class="icon-book-open"></i> Ayuda <span class="badge badge-danger">PDF</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="main.html"><i class="icon-info"></i> Acerca de...<span class="badge badge-info">IT</span></a>
-                </li>
-            </ul>
-        </nav>
-        <button class="sidebar-minimizer brand-minimizer" type="button"></button>
-    </div>
-
+    @include('template.sidebar')
     <!-- Contenido Principal -->
        @yield('content')
     <!-- /Fin del contenido principal -->
 </div>
 
-
+</div>
 
 <footer class="app-footer">
     <span><a href="http://www.incanatoit.com/">IncanatoIT</a> &copy; 2017</span>
     <span class="ml-auto">Desarrollado por <a href="http://www.incanatoit.com/">IncanatoIT</a></span>
 </footer>
 
-<!-- Bootstrap and necessary plugins -->
-<script src="js/jquery.min.js"></script>
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/pace.min.js"></script>
-<!-- Plugins and scripts required by all views -->
-<script src="js/Chart.min.js"></script>
-<!-- GenesisUI main scripts -->
+<script src="js/app.js"></script>
 <script src="js/template.js"></script>
 </body>
 
